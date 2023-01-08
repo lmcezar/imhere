9b9ddac3911ed08078cd852aeb9dce40f1ba071e
Lucas Cezar <lmcezar@gmail.com>
2023-01-08 10:46:05 -0300

    oranizando o projeto

--- App.tsx
+++ App.tsx
@@ -1,32 +1,8 @@
-import { Text, View, StyleSheet } from 'react-native'
+import Home from './src/screens/Home'
 
 export default function App() {
   return(
-    <View style={styles.container}>
-      <Text style={styles.eventName}>
-        React Native
-      </Text>
-      <Text style={styles.eventDate}>
-        08/01/2023
-      </Text>
-    </View>
+   <Home />
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#131016',
-    padding: 24
-  },
-  eventName: {
-    color: '#FDFCFE',
-    fontSize: 24,
-    marginTop: 48,
-    fontWeight: 'bold'
-  },
-  eventDate: {
-    color: '#FDFCFE',
-    fontSize: 16
-  }
-})
---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from 'react-native'
+import { styles } from './styles'
+
+export default function Home() {
+  return(
+    <View style={styles.container}>
+      <Text style={styles.eventName}>
+        React Native
+      </Text>
+      <Text style={styles.eventDate}>
+        08/01/2023
+      </Text>
+    </View>
+  )
+}
